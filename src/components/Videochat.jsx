0c0f12db12fa684e3158e65aa3b8c1e5f0071516
Felix <dexdaprotocol@gmail.com>
2022-07-13 14:53:14 +0200

    Update Videochat.jsx
    whereby embed

--- src/components/Videochat.jsx
+++ src/components/Videochat.jsx
@@ -1,3 +1,11 @@
 export default function Videochat() {
-  return <div>this is the videochat</div>;
+  return (
+    <div className="w-full items-center justify-center">
+      <iframe
+        className="h-screen w-full"
+        src="https://uplink.whereby.com/demo-5b838e9a-7a2b-4107-9e35-383a4a2d1f1d"
+        allow="camera; microphone; fullscreen; speaker; display-capture"
+      ></iframe>
+    </div>
+  );
 }
